f162434ce6256f35962a642b9e23d667bf4cb912
Beverlyakinyi <beverlyotienoakinyi@gmail.com>
2024-02-27 07:54:52 +0300

    added files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(){
+    println(totalSalary(9, 11))
+    println(totalSalary(4,67887))
+    println(totalSalary(8,6555))
+
+}
+fun totalSalary(numh:Int ,numhr:Int):Int{
+var totalsalary= numh * numhr
+    return totalsalary
+}
+
